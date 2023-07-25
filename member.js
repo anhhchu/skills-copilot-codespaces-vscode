bd9fbf3dbc929201318a2911d500e4b568c6d747
anhcodes <13889581+anhhchu@users.noreply.github.com>
2023-07-25 06:32:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS'];
+    var member = {
+        name: 'Lee',
+        age: 20,
+        skills: skills
+    };
+    console.log(member);
+}
